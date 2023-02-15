e6c94b62c1bd54c0f219177476f5f7f062b03f2c
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-02-15 19:24:18 +0530

    Given a list of integers, write a stream pipeline         that returns a list of the squares of the even integers         in the original list.]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        1.Given a list of integers, write a stream pipeline
+        that returns a list of the squares of the even integers
+        in the original list.
+         */
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> integerList = numbers.stream().filter(num -> num % 2 == 0)
+                .map(num -> num * num)
+                .collect(Collectors.toList());
+
+        integerList.forEach(System.out::println);
     }
 }
